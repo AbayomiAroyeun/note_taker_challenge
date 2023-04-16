c3b75ed919ec57714b0758f9bdb1488843708bd3
AbayomiAroyeun <aroyeunmichael@gmail.com>
2023-04-16 01:04:47 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# note_taker_challenge
+# Note Taker generating App
+This is a note take generating App otherwise called "emplyee note taker " for academic purpose.It's a note taking app that allows users to create, edit, and delete notes. The app is built with HTML, CSS, nodes and JavaScript. It works on a server-side API to Create, Read, Update, Delete operations on notes, making it available for users to make events.
+# Usage
+It uses basically Nodes, Javascript, html and css files.
+
+
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.get("/notes", (req, res)=>{
+res.sendFile(path.join(process.cwd(),"./public/notes.html" ))
+})
+
+//app.get("/profile", (req, res)=>{
+//res.json({"profile": "hello"})
+//})
+
+
+app.listen(PORT, ()=>{
+    console.log("port is running on http://localhost:"+PORT)
+})
